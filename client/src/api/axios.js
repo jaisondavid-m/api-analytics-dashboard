22d781c70595926a9a5ed300a8a81618b6c22e1b
jaisondavid-m <jaisondavidm.cs25@bitsathy.ac.in>
2026-04-11 19:11:22 +0530

    added backend url in frontend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"http://localhost:8000",
+    baseURL:"https://api-analytics-dashboard-xz14.onrender.com",
     withCredentials:true,
     headers:{
         "Content-Type":"application/json",
