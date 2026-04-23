76e7e1fa9f29982d27e13fdda0e5440bfc419da2
Jaison David M <jaisondavidm.cs25@bitsathy.ac.in>
2026-04-23 15:20:56 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL:"https://api-analytics-dashboard-xz14.onrender.com",
+    baseURL:"https://api-analytics-dashboard-0mvm.onrender.com",
     withCredentials:true,
     headers:{
         "Content-Type":"application/json",
